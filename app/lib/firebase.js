b64115e0151e13aec5e69149d30d20b9a4bd81da
CharlotteLekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-10-08 10:35:40 +0200

    updated the firebase configuration to use environment variables instead of hardcoded values

--- app/lib/firebase.js
+++ app/lib/firebase.js
@@ -1,19 +1,17 @@
-// Import the functions you need from the SDKs you need
+// firebaseConfig.js
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyAbOsafpWQYtCwjZArMESOAjEMZb5dDWeU",
-  authDomain: "fsj03-89680.firebaseapp.com",
-  projectId: "fsj03-89680",
-  storageBucket: "fsj03-89680.appspot.com",
-  messagingSenderId: "154171079401",
-  appId: "1:154171079401:web:ffe75278861d88c47cce78",
-  measurementId: "G-QG7ELEL8YK"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
