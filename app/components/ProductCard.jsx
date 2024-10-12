cb140cd4f3c37a6cacad6d012cb94e10f58f4806
CharlotteLekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-10-12 18:57:38 +0200

    editted the code on the product card file

--- app/components/ProductCard.jsx
+++ app/components/ProductCard.jsx
@@ -1,37 +1,64 @@
-// components/ProductCard.js
+"use client";
+import React from "react";
 import Link from "next/link";
-import ProductImageCarousel from "./ProductImageCarousel";
+import Image from "next/image";
+import favorite from "../public/favorite.svg";
+import cart from "../public/cart.svg";
 
-export default function ProductCard({ product }) {
+const ProductCard = (props) => {
+  const { title, thumbnail, price, category, id } = props;
+
   return (
-    <div className="bg-white overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300">
-      {/* Display carousel if there are multiple images */}
-      
-      {product.images.length > 1 ? (
-        <ProductImageCarousel images={product.images} />
-      ) : (
-        <img
-          src={product.images[0]}
-          alt={product.title}
-          className="h-40 w-full object-contain"
+    <div className="flex flex-col max-h-[130rem] cursor-pointer max-w-80 hover:-translate-y-1 hover:scale-105 duration-300 bg-white border border-slate-200 shadow shadow-slate-950/5  overflow-hidden">
+      <div className="flex align-center p-2">
+        <button className="relative left-[85%]">
+          {/*  */}
+          <Image width={30} height={30} src={favorite} alt="" />
+        </button>
+      </div>
+
+      <Link href={`/product/${id}`} className="flex bg-white justify-center">
+        <Image
+          className="object-cover "
+          priority="true"
+          alt={title}
+          src={thumbnail}
+          width={300}
+          height={300}
+          
         />
-      )}
-      
-      <div className="p-4">
-        <h2 className="text-lg font-semibold text-gray-800 truncate">
-          {product.title}
-        </h2>
-        <p className="text-grey-600 font-bold mt-2">${product.price}</p>
-        <p className="text-gray-500 text-sm">{product.category}</p>
-        
-        <Link
-          href={`/${product.id}`}
-          className="block mt-4 text-center px-4 py-2 bg-green-400
-           text-white rounded-lg hover:bg-green-500 transition-colors duration-300"
-        >
-          View Details
-        </Link>
+      </Link>
+
+      <div className="flex-1 flex flex-col p-2">
+        <div className="flex-1">
+          <header className="mb-2 flex-2">
+            <h2 className="text-sm line-clamp-2 font-extrabold leading-snug">
+              <div className="text-slate-600 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300">
+                <Link href="">{title}</Link>
+              </div>
+            </h2>
+          </header>
+
+          <div className="text-base line-clamp-2 font-extrabold text-slate-500 leading-snug">
+            <h2>R{price}</h2>
+          </div>
+        </div>
+
+        <div className="flex mt-1 space-x-2">
+          <div className="justify-start flex-1">
+            <span className="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
+              {category}
+            </span>
+          </div>
+        </div>
+        <div className="flex justify-end gap-3 space-x-2">
+          <button>
+            <Image className="w-8" width={100} height={100} src={cart} alt="" />
+          </button>
+        </div>
       </div>
     </div>
   );
-}
+};
+
+export default ProductCard;
